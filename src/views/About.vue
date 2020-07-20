07cbc82a71cdc5484954717b737e038f9d4e6dfe
salston89 <64949281+salston89@users.noreply.github.com>
2020-07-19 22:38:29 -0400

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>This is a todo test app built with Vue. It is part of Brad Traversy's Crash Course series on Vue.</p>
+    </div>
+</template>
+
+<style scoped>
+.about {
+    margin: 0 auto;
+    text-align: center;
+}
+.about h1 {
+    padding: 20px;
+}
+</style>
